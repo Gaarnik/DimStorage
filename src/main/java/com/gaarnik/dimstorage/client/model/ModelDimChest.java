c179275309cd13245ec068179913d88b2be58179
Gaarnik <TheGaarnik@gmail.com>
2014-05-31 09:41:56 +0200

    Added: Probably model (need to add animation & texturing)

--- src/main/java/com/gaarnik/dimstorage/client/model/ModelDimChest.java
+++ src/main/java/com/gaarnik/dimstorage/client/model/ModelDimChest.java
@@ -11,33 +11,76 @@ import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
 
-public class ModelDimChest extends ModelBase
-{
-	//fields
-	ModelRenderer base;
+public class ModelDimChest extends ModelBase {
+	// ****************************************************************
 
-	public ModelDimChest()
-	{
-		textureWidth = 64;
-		textureHeight = 32;
+	// ****************************************************************
+	ModelRenderer base;
+	ModelRenderer top1;
+	ModelRenderer top2;
+	ModelRenderer top3;
+	ModelRenderer top4;
+	ModelRenderer movable;
+
+	// ****************************************************************
+	public ModelDimChest() {
+		textureWidth = 128;
+		textureHeight = 128;
 
 		base = new ModelRenderer(this, 0, 0);
-		base.addBox(-8F, -8F, -8F, 14, 14, 14);
-		base.setRotationPoint(1F, 18F, 1F);
-		base.setTextureSize(64, 32);
+		base.addBox(0F, 0F, 0F, 16, 13, 16);
+		base.setRotationPoint(-8F, 11F, -8F);
+		base.setTextureSize(128, 128);
 		base.mirror = true;
 		setRotation(base, 0F, 0F, 0F);
+		top1 = new ModelRenderer(this, 66, 2);
+		top1.addBox(0F, 0F, 0F, 16, 3, 2);
+		top1.setRotationPoint(-8F, 8F, -8F);
+		top1.setTextureSize(128, 128);
+		top1.mirror = true;
+		setRotation(top1, 0F, 0F, 0F);
+		top2 = new ModelRenderer(this, 0, 32);
+		top2.addBox(0F, 0F, 0F, 2, 3, 14);
+		top2.setRotationPoint(6F, 8F, -6F);
+		top2.setTextureSize(128, 128);
+		top2.mirror = true;
+		setRotation(top2, 0F, 0F, 0F);
+		top3 = new ModelRenderer(this, 36, 32);
+		top3.addBox(0F, 0F, 0F, 2, 3, 14);
+		top3.setRotationPoint(-8F, 8F, -6F);
+		top3.setTextureSize(128, 128);
+		top3.mirror = true;
+		setRotation(top3, 0F, 0F, 0F);
+		top4 = new ModelRenderer(this, 66, 10);
+		top4.addBox(0F, 0F, 0F, 12, 3, 8);
+		top4.setRotationPoint(-6F, 8F, 0F);
+		top4.setTextureSize(128, 128);
+		top4.mirror = true;
+		setRotation(top4, 0F, 0F, 0F);
+		movable = new ModelRenderer(this, 70, 24);
+		movable.addBox(0F, 0F, 0F, 12, 1, 6);
+		movable.setRotationPoint(-6F, 8.533334F, -6F);
+		movable.setTextureSize(128, 128);
+		movable.mirror = true;
+		setRotation(movable, 0F, 0F, 0F);
 	}
 
-	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5)
-	{
+	// ****************************************************************
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
 		super.render(entity, f, f1, f2, f3, f4, f5);
 		setRotationAngles(f, f1, f2, f3, f4, f5, entity);
+
 		base.render(f5);
+		top1.render(f5);
+		top2.render(f5);
+		top3.render(f5);
+		top4.render(f5);
+		movable.render(f5);
 	}
 
-	private void setRotation(ModelRenderer model, float x, float y, float z)
-	{
+	// ****************************************************************
+	private void setRotation(ModelRenderer model, float x, float y, float z) {
 		model.rotateAngleX = x;
 		model.rotateAngleY = y;
 		model.rotateAngleZ = z;
