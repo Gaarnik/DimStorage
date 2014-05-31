cf4b8c63bdc527ed8e4c563cc3d41b11f2b06111
Gaarnik <TheGaarnik@gmail.com>
2014-05-31 10:37:47 +0200

    Added: textures for DimChest model and inventory icon

--- src/main/java/com/gaarnik/dimstorage/common/BlockDimChest.java
+++ src/main/java/com/gaarnik/dimstorage/common/BlockDimChest.java
@@ -2,10 +2,12 @@ package com.gaarnik.dimstorage.common;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -18,17 +20,18 @@ import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockDimChest extends BlockContainer {
 	// ****************************************************************
-	
+
 	// ****************************************************************
+	private Icon top, sides;
 
 	// ****************************************************************
 	public BlockDimChest(int id) {
 		super(id, Material.rock);
-		
+
 		this.setHardness(20F);
 		this.setResistance(100F);
 		this.setStepSound(soundStoneFootstep);
-		
+
 		this.setCreativeTab(DimStorage.tabDimStorage);
 	}
 
@@ -43,20 +46,33 @@ public class BlockDimChest extends BlockContainer {
 
 		return true;
 	}
-	
+
 	@Override
 	public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase player, ItemStack stack) {
 		int direction =  (MathHelper.floor_double((double)(player.rotationYaw * 4.0F / 360.0F) + 2.5D) & 3) + 2;
 		TileEntity te = world.getBlockTileEntity(x, y, z);
-		if(te instanceof TEDimChest)
-		{
+
+		if(te != null && te instanceof TEDimChest) {
 			((TEDimChest) te).setDirection((byte)direction);;
 			world.markBlockForUpdate(x, y, z);
 		}
-//        world.setBlockMetadataWithNotify(x, y, z, direction, 0);
 	}
 
 	// ****************************************************************
+	@Override
+	public void registerIcons(IconRegister iconRegister) {
+		this.top = iconRegister.registerIcon(DimStorage.MODID + ":dimchest_top");
+		this.sides = iconRegister.registerIcon(DimStorage.MODID + ":dimchest_sides");
+	}
+
+	@Override
+	public Icon getIcon(int side, int metadata) {
+		switch(side) {
+		case 1: return  this.top;
+		default: return this.sides;
+		}
+	}
+
 	@SideOnly(Side.CLIENT)
 	@Override
 	public boolean shouldSideBeRendered(IBlockAccess par1iBlockAccess, int par2, int par3, int par4, int par5) {
@@ -68,13 +84,9 @@ public class BlockDimChest extends BlockContainer {
 	public boolean isOpaqueCube() {
 		return false;
 	}
-	
+
 	// ****************************************************************
-	
-	public void setBlockBoundsBasedOnState(IBlockAccess par1IBlockAccess, int par2, int par3, int par4) {
-		this.setBlockBounds(0.1F, 0.0F, 0.1F, 0.9F, 0.8F, 0.9F);
-	}
-	
+
 	// ****************************************************************
 	@Override
 	public TileEntity createNewTileEntity(World world) { return new TEDimChest(); }
