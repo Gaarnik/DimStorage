b087fe1cc89feed83e9301c6a9d40745fde78262
Gaarnik <TheGaarnik@gmail.com>
2014-06-13 18:05:51 +0200

    Fixed; import missing

--- src/main/java/com/gaarnik/dimstorage/DimStorage.java
+++ src/main/java/com/gaarnik/dimstorage/DimStorage.java
@@ -14,6 +14,7 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.relauncher.Side;
